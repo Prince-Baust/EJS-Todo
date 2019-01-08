144eba3d82d918e81b4d163239060c7b867862e3
Prince <pmaruf42@gmail.com>
2019-01-09 01:50:57 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,49 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
+const items = [];
+const works = [];
+
+
+app.get("/", function(req, res){
+    const day = date.getDay();
+
+    res.render("list", {listTitle: day, listItems: items});
+
+});
+
+app.post("/", function (req, res) {
+    let item = req.body.newItem;
+    if (req.body.list === "Work"){
+        works.push(item);
+        res.redirect("/work");
+    }else{
+        items.push(item);
+        res.redirect("/");
+    }
+
+});
+
+app.get("/work", function (req, res) {
+   res.render("list", {listTitle: "Work", listItems: works});
+});
+
+app.post("/work", function (req, res) {
+    res.redirect("/work");
+});
+
+
+app.get("/about", function (req, res) {
+   res.render("about");
+});
+app.listen(3000, function(){
+    console.log("Server started on port 3000.");
+});
